6646faa150d475f2e88b22ba4c2befb9cc0d7ec6
danielemontesi <daniele.montesi@iubar.it>
2017-01-19 12:56:18 +0100

    .

--- src/Iubar/Login/Interfaces/IRegistration.php
+++ src/Iubar/Login/Interfaces/IRegistration.php
@@ -4,6 +4,6 @@ namespace Iubar\Login\Interfaces;
 
 interface IRegistration {
 	
-	public function writeNewUserToDatabase($user_name, $user_password_hash, $user_email, $user_activation_hash, $provider_type);
+	public function writeNewUserToDatabase($user);
 				
 }
