f2d9dec134d360c0d33a57a6ca5cbf317e81e958
daniele <daniele.montesi@iubar.it>
2017-04-07 11:37:20 +0200

    .

--- src/Iubar/Login/Services/Text.php
+++ src/Iubar/Login/Services/Text.php
@@ -1,17 +1,15 @@
 <?php
-
 namespace Iubar\Login\Services;
 
 class Text {
-	
+
 	private static $texts = null;
 	
 	// Esempio: se la stringa nel file eng.php è:
-	// "FEEDBACK_USER_EMAIL_ALREADY_TAKEN" => "Sorry, the email $user_email is already in use." 
+	// "FEEDBACK_USER_EMAIL_ALREADY_TAKEN" => "Sorry, the email $user_email is already in use."
 	// posso invocare il metodo:
 	// Text::get('FEEDBACK_USER_EMAIL_ALREADY_TAKEN', ['user_email' => $user_email])
-
-	public static function get($key, $data=null){
+	public static function get($key, $data = null) {
 		
 		// if not $key
 		if (!$key) {
@@ -23,14 +21,14 @@ class Text {
 				${$var} = $value;
 			}
 		}
-				
+		
 		// load config file (this is only done once per application lifecycle)
 		if (!self::$texts) {
 			$file = __DIR__ . '/translations/it.php';
-			if(is_readable($file)){
-				self::$texts = require($file);						
-			}else{						
-				throw new \RuntimeException('Can not load the translation file: ' . $file);		 
+			if (is_readable($file)) {
+				self::$texts = require ($file);
+			} else {
+				throw new \RuntimeException('Can not load the translation file: ' . $file);
 			}
 		}
 		// check if array key exists
