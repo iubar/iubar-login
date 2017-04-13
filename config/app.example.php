9974a9986f44b78a7dfedae88d507600f9822448
daniele <daniele.montesi@iubar.it>
2017-04-13 10:36:53 +0200

    Edit example config

--- config/app.example.php
+++ config/app.example.php
@@ -5,7 +5,10 @@ return array (
 	// App
 	'app.name' => 'app name here',
 	'app.templates.path' => 'app',
-    
+
+	// Auth
+	'go.secret.file' => __DIR__ . '/google-client-secret.json',
+
     // Email
     'email.mailgun.password' => 'password',
     'email.smtp' => 'smpt',
